51649e86dd1b5e33e098f20edf8860ec3a45347c
Neander de Souza <neander.faria@hotmail.com>
2023-10-25 14:52:17 -0300

    Teste de integração - Testando o Search

---
+++ src/screens/Search/search.test.tsx
@@ -0,0 +1,21 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
+import { fireEvent, render, screen, waitFor } from "@__tests__/utils/customRender"
+
+import { Search } from "@screens/Search"
+
+import { api } from "@services/api"
+
+describe('Screen: Search', () => {
+    it('should be show city option', async () => {
+        jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
+
+        render(<Search />)
+
+        const searchInput = screen.getByTestId('search-input')
+        fireEvent.changeText(searchInput, "São Paulo")
+
+        const option = await waitFor(() => screen.findByText(/são paulo/i))
+
+        expect(option).toBeTruthy()
+    })
+})
