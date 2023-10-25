392bff77ec7c4ced04a5921c9ab6c93f8b99179e
Neander de Souza <neander.faria@hotmail.com>
2023-10-25 14:53:49 -0300

    Teste de integração - Multiplas requisições

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,3 +1,4 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
 import { render, screen, waitFor } from "@__tests__/utils/customRender"
 
@@ -24,5 +25,21 @@ describe("Screen: Dashboard", () => {
 
         const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
         expect(cityName).toBeTruthy()
+    })
+
+    it('should be show another selected weather city', async () => {
+        const city = {
+            id: '1',
+            name: 'Rio do Sul, BR',
+            latitude: 123,
+            longitude: 456
+        }
+
+        await saveStorageCity(city)
+
+        jest.spyOn(api, 'get')
+            .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+            .mockResolvedValueOnce({ data: mockCityAPIResponse })
+            .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
     })
 })
