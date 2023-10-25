ac157504989fb60a20e6ae7a30227f6afd893d81
Neander de Souza <neander.faria@hotmail.com>
2023-10-25 13:57:04 -0300

    Mocks - fireEvent

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -1,25 +1,24 @@
-import { render, screen } from "@testing-library/react-native"
+import { fireEvent, render, screen } from "@testing-library/react-native"
 
 import { SelectList } from "@components/SelectList"
 
-import { CityProps } from "@services/getCityByNameService"
+describe("Component: SelectList", () => {
+    it('should be return city details selected', async () => {
+        const data = [
+            { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+            { id: '2', name: 'Campo grande', latitude: 123, longitude: 456 }
+        ]
 
-describe("Component: SelectList", () =>{
-  it('should be return city details selected', async() => {
-    const data = [
-      { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
-      { id: '2', name: 'Campo grande', latitude: 123, longitude: 456 }
-    ]
+        render(
+            <SelectList
+                data={data}
+                onChange={() => { }}
+                onPress={() => { }}
+            />
+        )
 
-    render(
-      <SelectList 
-        data={data}
-        onChange={() =>{}}
-        onPress={() => {}}
-      />
-    )
+        const selectedCity = screen.getByText(/campo/i)
 
-    const selectedCity = screen.getByText(/campo/i)
-    console.log(selectedCity)
-  })
+        fireEvent.press(selectedCity)
+    })
 })
